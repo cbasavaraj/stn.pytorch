aa3fe0202e0590f5ae5a2a8ed3e6f96103408230
CBasavaraj <chandrachud.basavaraj@gmail.com>
2017-12-27 22:19:04 +0100

    edits: minor

--- script/test.py
+++ script/test.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 
-import torch
 import numpy as np
+
+import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
@@ -11,6 +12,7 @@ from modules.gridgen import AffineGridGen, CylinderGridGen, CylinderGridGenV2, D
 import time
 
 nframes = 64
+
 height = 64
 width = 128
 channels = 64
@@ -21,12 +23,12 @@ grids = torch.zeros(nframes, height, width, 2)
 input1, input2 = Variable(inputImages, requires_grad=True), Variable(grids, requires_grad=True)
 
 input1.data.uniform_()
-input2.data.uniform_(-1,1)
+input2.data.uniform_(-1, 1)
 
-input = Variable(torch.from_numpy(np.array([[[0.8, 0.3, 1], [0.5, 0, 0]]], dtype=np.float32)), requires_grad = True)
+input = Variable(torch.from_numpy(np.array([[[0.8, 0.3, 1], [0.5, 0, 0]]], dtype=np.float32)), requires_grad=True)
 print(input)
 
-g = AffineGridGen(64, 128, aux_loss = True)
+g = AffineGridGen(64, 128, aux_loss=True)
 out, aux = g(input)
 print((out.size()))
 out.backward(out.data)
